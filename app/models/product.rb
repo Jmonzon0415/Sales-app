ee9dae3a36203291f46549012b25b613ebf04ee3
Joshua Monzon <Jmonzon0415@gmail.com>
2016-01-20 21:24:42 -0500

    Adds migration exercise

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,25 @@ class Product < ActiveRecord::Base
     updated_at.strftime("%b %e, %Y" )
   end  
 
+  
+  def sale_message 
+    if price.to_f < 2.0  
+       "Discount Item!!" 
+    else 
+       "On sale!!!"
+    end
+  end
+
+  
+  def tax
+    tax = price.to_f * 0.09
+  end
+
+  
+  def total_sum
+    total = price.to_f + tax 
+    p "$#{total.to_f.round(2)}" 
+  end 
+
 
 end
